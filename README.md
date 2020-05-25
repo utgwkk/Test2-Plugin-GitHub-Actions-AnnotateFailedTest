0eadad52e2278c82dcffb7dcd7beabfd3294d3e7
utgwkk <utagawakiki@gmail.com>
2020-05-25 16:41:45 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test2::Plugin::GitHub::Actions::AnnotateFailedTest - It's new $module
+
+# SYNOPSIS
+
+    use Test2::Plugin::GitHub::Actions::AnnotateFailedTest;
+
+# DESCRIPTION
+
+Test2::Plugin::GitHub::Actions::AnnotateFailedTest is ...
+
+# LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+utgwkk <utagawakiki@gmail.com>
---
+++ lib/Test2/Plugin/GitHub/Actions/AnnotateFailedTest.pm
@@ -0,0 +1,39 @@
+package Test2::Plugin::GitHub::Actions::AnnotateFailedTest;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test2::Plugin::GitHub::Actions::AnnotateFailedTest - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test2::Plugin::GitHub::Actions::AnnotateFailedTest;
+
+=head1 DESCRIPTION
+
+Test2::Plugin::GitHub::Actions::AnnotateFailedTest is ...
+
+=head1 LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+utgwkk E<lt>utagawakiki@gmail.comE<gt>
+
+=cut
+
