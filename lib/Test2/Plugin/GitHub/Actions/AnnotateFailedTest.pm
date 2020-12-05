841b15536ae3141570f4a5975d34c2552396451e
FUJI Goro <goro@fastly.com>
2020-12-05 10:33:15 +0900

    allow annotate itself

--- lib/Test2/Plugin/GitHub/Actions/AnnotateFailedTest.pm
+++ lib/Test2/Plugin/GitHub/Actions/AnnotateFailedTest.pm
@@ -15,14 +15,15 @@ our $VERSION = "0.04";
 
 sub import {
     my ($class) = @_;
-
     return unless $ENV{GITHUB_ACTIONS};
-    state $loaded = 0; # avoid multiple callback addition
-    return if $loaded;
-    $loaded++;
+
+    state %loaded ; # avoid multiple callback additions per Test2::Hub
 
     test2_add_callback_post_load(sub {
         my $hub = test2_stack()->top;
+        return if $loaded{$hub->hid};
+        $loaded{$hub->hid}++;
+
         $hub->listen(\&listener, inherit => 1);
     });
 }
